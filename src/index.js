4d91b8427ab73013904dcee8ac0557833e70ad54
Fatima Khalife <FatimaElZahraa@Fatimas-MacBook-Pro.local>
2023-06-11 20:43:20 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form');
+  const inputField = document.getElementById('new-task-description');
+  const taskList = document.getElementById('tasks');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const taskDescription = inputField.value;
+    if (taskDescription.trim() !== '') {
+      const li = document.createElement('li');
+      const text = document.createTextNode(taskDescription);
+      li.appendChild(text);
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = 'Delete';
+      deleteButton.addEventListener('click', () => {
+        li.remove();
+      });
+      li.appendChild(deleteButton);
+      taskList.appendChild(li);
+
+      inputField.value = '';
+    }
+  });
 });
